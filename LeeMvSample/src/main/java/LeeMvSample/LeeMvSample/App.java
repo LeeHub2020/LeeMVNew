baa4368e824c8e8b5646e7098d7528c2da72cae7
leenasaindane@gmail.com <Lee@github20>
2019-12-01 20:49:18 +0530

    commit1

---
+++ LeeMvSample/src/main/java/LeeMvSample/LeeMvSample/App.java
@@ -0,0 +1,28 @@
+package LeeMvSample.LeeMvSample;
+
+/**
+ * Hello world!
+ *
+ */
+//import java.io.IOException;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	//int inChar;
+    	//System.out.println("Enter a character :");
+    	//try
+    	//{
+    		//inChar = System.in.read();
+    		//System.out.print("You entered :");
+    		//System.out.println(inChar);
+    	//}
+    	//catch (IOException e)
+    	//{
+    		System.out.println( "Hello World" );
+    		System.out.print("Hello Eclipse");
+    	//}
+    
+    }
+}
